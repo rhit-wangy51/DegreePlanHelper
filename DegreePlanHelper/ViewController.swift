7dbcd7417c1f12da0e5e8f86c0d91eb489f997b3
rhit-wangy51 <wangy51@rose-hulman.edu>
2022-04-26 14:16:12 -0400

    Initial Commit

---
+++ DegreePlanHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DegreePlanHelper
+//
+//  Created by Helen Wang on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
